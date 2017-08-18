0f59140ff654e0f735349f827e1e07ce5c5d2318
Leonardo Val <leonardo.val@creatartis.com>
2017-08-18 11:40:43 -0300

    First test cases
    + Player for TicTacToe based on a randomly initialized linear classifier with game actions as outputs.

---
+++ tests/karma-tester.js
@@ -0,0 +1,21 @@
+(function () {
+	require.config({ // Configure RequireJS.
+		baseUrl: '/base',
+		paths: {
+			'creatartis-base': '/base/tests/lib/creatartis-base.min',
+			'sermat': '/base/tests/lib/sermat-umd',
+			'ludorum': '/base/tests/lib/ludorum.min',
+			'ludorum-player-ml': '/base/tests/lib/ludorum-player-ml'
+		}
+	});
+	require(Object.keys(window.__karma__.files) // Dynamically load all test files
+			.filter(function (file) { // Filter test modules.
+				return /\.test\.js$/.test(file);
+			}).map(function (file) { // Normalize paths to RequireJS module names.
+				return file.replace(/^\/base\//, '').replace(/\.js$/, '');
+			}),
+		function () {
+			window.__karma__.start(); // we have to kickoff jasmine, as it is asynchronous
+		}
+	);
+})();
---
+++ tests/specs/players.test.js
@@ -0,0 +1,62 @@
+﻿define(['creatartis-base', 'ludorum', 'ludorum-player-ml'], function (base, ludorum, ludorum_player_ml) {
+	var declare = base.declare,
+		Iterable = base.Iterable,
+		Match = ludorum.Match,
+		GameModel = ludorum_player_ml.GameModel,
+		GameClassifier = ludorum_player_ml.GameClassifier;
+
+	var TicTacToeGameModel = declare(GameModel, {
+		constructor: function TicTacToeGameModel(game) {
+			GameModel.call(this, game);
+		},
+
+		__possibleActions__: [0,1,2,3,4,5,6,7,8],
+
+		__featureRanges__: Iterable.repeat({ min: -1, max: 1 }, 9).toArray(),
+
+		features: function features(game, player) {
+			var players = game.players.map(function (p) {
+					return p.charAt(0);
+				}),
+				factor = player === players[0] ? +1 : -1;
+			return game.board.split('').map(function (sq) {
+				return (sq === players[0]) ? factor : (sq === players[1]) ? -factor : 0;
+			});
+		}
+	}); // declare TicTacToeGameModel
+
+	var TICTACTOE_MODEL = new TicTacToeGameModel(new ludorum.games.TicTacToe());
+
+	function randomClassifier(ClassifierType) {
+		var random = ClassifierType.prototype.random,
+			parameters = ClassifierType.prototype.parameterRanges.map(function (r) {
+				return random.random(r.min, r.max);
+			});
+		return new ClassifierType(parameters);
+	}
+
+	describe("LinearClassifier", function () { ////////////////////////////////////////////////////
+		var LinearClassifier = ludorum_player_ml.LinearClassifier,
+			ActionClassifierPlayer = ludorum_player_ml.ActionClassifierPlayer;
+
+		it("is properly configured", function () {
+			expect(typeof LinearClassifier).toBe('function');
+			expect(LinearClassifier.prototype instanceof GameClassifier).toBe(true);
+		});
+
+		it("with ActionClassifierPlayer", function (done) {
+			var ActionLinearClassifier = LinearClassifier.actionClassifier(TICTACTOE_MODEL);
+			expect(ActionLinearClassifier.prototype instanceof LinearClassifier).toBe(true);
+			var linearClassifier = randomClassifier(ActionLinearClassifier);
+			expect(linearClassifier instanceof ActionLinearClassifier).toBe(true);
+			var player = new ActionClassifierPlayer({ classifier: linearClassifier }),
+				match = new Match(TICTACTOE_MODEL.game, [player, player]);
+			match.run().then(function () {
+				expect(match.result()).toBeTruthy();
+				done();
+			});
+		});
+
+	}); //// LinearClassifier
+
+}); //// define.
