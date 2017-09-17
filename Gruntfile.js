3df83ac839edc00eae50bb7accdd7d4e0296110d
Leonardo Val <leonardo.val@creatartis.com>
2017-09-17 09:34:12 -0300

    Updated `creatartis-grunt`

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,10 @@ module.exports = function (grunt) {
 				'core', 'players', 'linear-classifiers', 'training',
 			'__epilogue__'],
 		deps: [
-			{ name: 'creatartis-base', id: 'base',
-				path: 'node_modules/creatartis-base/build/creatartis-base.min.js' },
-			{ name: 'sermat', id: 'Sermat',
+			{ id: 'creatartis-base', name: 'base' },
+			{ id: 'sermat', name: 'Sermat',
 				path: 'node_modules/sermat/build/sermat-umd.js' },
-			{ name: 'ludorum', id: 'ludorum',
-				path: 'node_modules/ludorum/build/ludorum.js' }
+			{ id: 'ludorum' }
 		],
 		otherCopy: [
 			{ src: 'node_modules/@creatartis/ludorum-game-mancala/build/ludorum-game-mancala.js',
