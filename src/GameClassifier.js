e7d5c5ef07b5b26190b22dcc6711b4af2b6f6b65
Leonardo Val <leonardo.val@creatartis.com>
2018-04-14 16:10:28 -0300

    Generalized `GameClassifier`
    + Added methods `classify` and `matches`, to include different classification schemes.
    + Parametrical classifiers where moved to a subclass.

---
+++ src/GameClassifier.js
@@ -0,0 +1,116 @@
+/** # Game classifier
+
+A game classifier is a classifier that takes its inputs from a game state and a player. The outputs
+(classes) can be different concepts, depending on the way the classifier is meant to be used.
+*/
+var GameClassifier = exports.GameClassifier = declare({
+	/** The base `GameClassifier` type is not meant to be used, but rather to be extended. The base
+	constructor may take the following arguments:
+	*/
+	constructor: function GameClassifier(args) {
+		initialize(this, args)
+		/** + `gameModel`: the game model that defines features and classes for the games to be 
+			classified. 
+		*/
+			.object('gameModel', { ignore: true })
+		/** + `classes` is a list of values, each belonging to one of the classes in which the 
+			game states are supposed to be classified.
+		*/
+			.array('classes', { ignore: true })
+		/** + `random`: a pseudo-random number generator, as a `base.Randomness` instance.
+		*/
+			.object('random', { ignore: true })
+		;
+	},
+
+	/** The `gameModel` is used to calculate features and other information from game states.
+	*/
+	gameModel: null,
+
+	/** No classes are defined by default. */
+	classes: [],
+
+	/** The default value for `random` is the default is the default `base.Randomness` singleton.
+	This is not seeded. It is adviced to change this when performing simulations or training.
+	*/
+	random: base.Randomness.DEFAULT,
+
+	// ## Classification ##########################################################################
+
+	/** The base implementation returns a class at random. Only useful for testing purposes.
+	*/
+	classify: function classify(game, player) {
+		var matches = this.matches(game, player);
+		return matches.length > 0 ? this.random.choice(matches) : null;
+	},
+
+	/** Simple classification that returns the first class that matches the `game` state.
+	*/
+	classify_firstMatch: function classify_firstMatch(game, player) {
+		var matches = this.matches(game, player);
+		return matches.length > 0 ? matches[0] : null;
+	},
+
+	// ## Matching ################################################################################
+
+	/** Although the function of a classifier is to choose one and only one class for a given game
+	state, sometimes it may be useful to know whether many classes can apply. The `matches` method
+	returns a list of classes that may apply to the given `game` state. By default it returns all
+	possible classes.
+	*/
+	matches: function matches(game, player) {
+		return this.classes;
+	},
+
+	/** If the `evaluate` method is implemented, the `match_bestEvaluated` method returns an array
+	with the best evaluated classes.
+	*/
+	match_bestEvaluated: function match_bestEvaluated(game, player) {
+		var classes = this.classes;
+		return iterable(this.evaluate(game, player)).greater(function (clazz) {
+			return clazz[1];
+		}).map(function (clazz) {
+			return classes[clazz[0]];
+		}).toArray();
+	},
+
+	//TODO matchRule: function matchRule(values, game, player)
+
+	// ## Evaluation ##############################################################################
+
+	/** To `evaluate` a classifier is to calculate a list of pairs `[class, n]`, where `n` is a
+	numerical evaluation of the membership of the given `game` state to the corresponding `class`.
+	The greater the evaluation, the most likely the game state is to belong to the class.
+	*/
+	evaluate: function evaluate(game, player) {
+		var random = this.random;
+		return this.classes.map(function (clazz) {
+			return [clazz, random.random()];
+		});
+	},
+
+	/** The normalization makes the evaluations for all classes fit in the [0,1] range, adding up
+	to 1.
+	*/
+	normalizedEvaluate: function normalizedEvaluate(game, player) {
+		var evals = iterable(this.evaluate(game, player)),
+			min = +Infinity,
+			max = -Infinity,
+			sum = 0,
+			count = 0;
+		evals.forEachApply(function (i, v) {
+			if (min > v) {
+				min = v;
+			}
+			if (max < v) {
+				max = v;
+			}
+			sum += v;
+			count++;
+		});
+		sum = sum - count * min;
+		return evals.mapApply(function (i, v) {
+			return [i, (v - min) / sum];
+		}).toArray();
+	}
+}); // declare GameClassifier
