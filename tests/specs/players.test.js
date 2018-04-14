4743c2bbb435ebf0f3417f1aa600b5c7d1c92f4e
Leonardo Val <leonardo.val@creatartis.com>
2018-04-14 16:15:43 -0300

    Testing
    + Using the game model for Tictactoe included in the library.
    + Renamed `LinearClassifier` to `LinearGameClassifier`.

--- tests/specs/players.test.js
+++ tests/specs/players.test.js
@@ -5,52 +5,24 @@
 		GameModel = ludorum_player_ml.GameModel,
 		GameClassifier = ludorum_player_ml.GameClassifier;
 
-	var TicTacToeGameModel = declare(GameModel, {
-		constructor: function TicTacToeGameModel(game) {
-			GameModel.call(this, game || new ludorum.games.TicTacToe());
-		},
+	var TICTACTOE_MODEL = new ludorum_player_ml.examples.TicTacToeGameModel();
 
-		__actionClasses__: [0,1,2,3,4,5,6,7,8],
-
-		__featureRanges__: Iterable.repeat({ min: -1, max: 1 }, 9).toArray(),
-
-		features: function features(game, player) {
-			var players = game.players.map(function (p) {
-					return p.charAt(0);
-				}),
-				factor = player === players[0] ? +1 : -1;
-			return game.board.split('').map(function (sq) {
-				return (sq === players[0]) ? factor : (sq === players[1]) ? -factor : 0;
-			});
-		}
-	}); // declare TicTacToeGameModel
-
-	var TICTACTOE_MODEL = new TicTacToeGameModel();
-
-	function randomClassifier(ClassifierType) {
-		var random = ClassifierType.prototype.random,
-			parameters = ClassifierType.prototype.parameterRanges.map(function (r) {
-				return random.random(r.min, r.max);
-			});
-		return new ClassifierType(parameters);
-	}
-
-	describe("LinearClassifier", function () { ////////////////////////////////////////////////////
-		var LinearClassifier = ludorum_player_ml.LinearClassifier,
+	describe("LinearGameClassifier", function () { ////////////////////////////////////////////////////
+		var LinearGameClassifier = ludorum_player_ml.LinearGameClassifier,
 			ActionClassifierPlayer = ludorum_player_ml.ActionClassifierPlayer,
 			ResultClassifierPlayer = ludorum_player_ml.ResultClassifierPlayer;
 
 		it("is properly configured", function () {
-			expect(typeof LinearClassifier).toBe('function');
-			expect(LinearClassifier.prototype instanceof GameClassifier).toBe(true);
+			expect(typeof LinearGameClassifier).toBe('function');
+			expect(LinearGameClassifier.prototype instanceof GameClassifier).toBe(true);
 		});
 
 		it("with ActionClassifierPlayer", function (done) {
-			var ActionLinearClassifier = LinearClassifier.actionClassifier(TICTACTOE_MODEL);
-			expect(ActionLinearClassifier.prototype instanceof LinearClassifier).toBe(true);
-			var linearClassifier = randomClassifier(ActionLinearClassifier);
-			expect(linearClassifier instanceof ActionLinearClassifier).toBe(true);
-			var player = new ActionClassifierPlayer({ classifier: linearClassifier }),
+			var ActionLinearClassifier = LinearGameClassifier.actionClassifier(TICTACTOE_MODEL);
+			expect(ActionLinearClassifier.prototype instanceof LinearGameClassifier).toBe(true);
+			var linearGameClassifier = ActionLinearClassifier.randomClassifier();
+			expect(linearGameClassifier instanceof ActionLinearClassifier).toBe(true);
+			var player = new ActionClassifierPlayer({ classifier: linearGameClassifier }),
 				match = new Match(TICTACTOE_MODEL.game, [player, player]);
 			match.run().then(function () {
 				expect(match.result()).toBeTruthy();
@@ -59,17 +31,17 @@
 		});
 
 		it("with ResultClassifierPlayer", function (done) {
-			var ResultLinearClassifier = LinearClassifier.resultClassifier(TICTACTOE_MODEL);
-			expect(ResultLinearClassifier.prototype instanceof LinearClassifier).toBe(true);
-			var linearClassifier = randomClassifier(ResultLinearClassifier);
-			expect(linearClassifier instanceof ResultLinearClassifier).toBe(true);
-			var player = new ResultClassifierPlayer({ classifier: linearClassifier }),
+			var ResultLinearClassifier = LinearGameClassifier.resultClassifier(TICTACTOE_MODEL);
+			expect(ResultLinearClassifier.prototype instanceof LinearGameClassifier).toBe(true);
+			var linearGameClassifier = ResultLinearClassifier.randomClassifier();
+			expect(linearGameClassifier instanceof ResultLinearClassifier).toBe(true);
+			var player = new ResultClassifierPlayer({ classifier: linearGameClassifier }),
 				match = new Match(TICTACTOE_MODEL.game, [player, player]);
 			match.run().then(function () {
 				expect(match.result()).toBeTruthy();
 				done();
 			});
 		});
-	}); //// LinearClassifier
+	}); //// LinearGameClassifier
 
 }); //// define.
